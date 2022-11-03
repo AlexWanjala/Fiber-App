73fe438360c8fe5c2d8aafac72ed147c6e62d3eb
Mac <mac@Alexs-MacBook-Pro.local>
2022-11-03 21:14:30 +0300

    shdhd

---
+++ app/src/main/java/com/zuku/smartbill/zukufiber/ui/landing/OTP.kt
@@ -0,0 +1,12 @@
+package com.zuku.smartbill.zukufiber.ui.landing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zuku.smartbill.zukufiber.R
+
+class OTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
