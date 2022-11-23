d5bb08654964ecd10e9d0fcf81f2c2e698749b12
Mac <mac@Alexs-MacBook-Pro.local>
2022-11-23 15:41:03 +0300

    Login

--- app/src/main/java/com/zuku/smartbill/zukufiber/ui/Login.kt
+++ app/src/main/java/com/zuku/smartbill/zukufiber/ui/Login.kt
@@ -1,8 +1,10 @@
 package com.zuku.smartbill.zukufiber.ui
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.zuku.smartbill.zukufiber.R
 import kotlinx.android.synthetic.main.activity_login2.*
 
@@ -13,5 +15,24 @@ class Login : AppCompatActivity() {
 
         register.setOnClickListener { startActivity(Intent(this,com.zuku.smartbill.zukufiber.ui.landing.Login::class.java)) }
         login.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
+
+        whatsapp.setOnClickListener {
+            val url = "https://api.whatsapp.com/send?phone=254777320323"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+        call.setOnClickListener {
+            val dialIntent = Intent(Intent.ACTION_DIAL)
+            dialIntent.data = Uri.parse("tel:" + "0205222222")
+            startActivity(dialIntent)
+        }
+        info.setOnClickListener { openWebPage("https://zuku.co.ke") }
+        faq.setOnClickListener { openWebPage("https://zuku.co.ke/faq") }
+    }
+
+    fun Activity.openWebPage(url: String?) = url?.let {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+        if (intent.resolveActivity(packageManager) != null) startActivity(intent)
     }
 }
